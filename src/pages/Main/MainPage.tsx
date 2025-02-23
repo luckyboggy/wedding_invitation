1b60bc473d269a03df3433705f68b4c2acff0b07
unknown <luckyboggy@gmail.com>
2025-02-23 20:54:31 +0500

    timer

--- src/pages/Main/MainPage.tsx
+++ src/pages/Main/MainPage.tsx
@@ -1,5 +1,17 @@
 import React from "react";
+import cls from "./MainPage.module.scss";
+import Greating from "../../widgets/Greetings/ui/Greating";
+import Timer from "../../widgets/Timer/ui/Timer";
+import DressCode from "../../widgets/DressCode/ui/DressCode";
 
 export default function MainPage() {
-  return <div>Main</div>;
+  return (
+    <div className={cls.main}>
+      <div className={cls.container}>
+        <Greating />
+        <Timer />
+        <DressCode />
+      </div>
+    </div>
+  );
 }
--- src/widgets/Greetings/ui/Greating.tsx
+++ src/widgets/Greetings/ui/Greating.tsx
@@ -2,5 +2,22 @@ import React from "react";
 import cls from "./Greating.module.scss";
 
 export default function Greating() {
-  return <div>Greating</div>;
+  return (
+    <div className={cls.greating}>
+      <div className={cls.wrapper}>
+        <div className={cls.mainGreating}>
+          
+          <h1>Дорогие друзья и родные</h1>
+        </div>
+        <div className={cls.greatingText}>
+          <p>Мы приглашаем вас разделить с нами....</p>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Facilis
+            earum labore totam quasi magnam ipsam mollitia esse, quia excepturi?
+            Natus!
+          </p>
+        </div>
+      </div>
+    </div>
+  );
 }
